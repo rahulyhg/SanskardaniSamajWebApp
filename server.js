c9ca107c5d1917eaacc80787a96b0c96f5ef6280
rakeshise <rakeshise@gmail.com>
2019-01-30 17:32:22 +0530

    make it better

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/SanskardaniSamajWebApp'));
 
 app.listen(process.env.PORT||8080);
 
 //PathLocationStradegy
 
 app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname,'src', 'index.html'));
+    res.sendFile(path.join(__dirname,'/dist/SanskardaniSamajWebApp/index.html'));
 });
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'src', 'index.html'));
+  res.sendFile(path.join(__dirname,'/dist/SanskardaniSamajWebApp/index.html'));
 });
 
 console.log('Console Listening'); 
