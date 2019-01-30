bd009836d8d3ba0131753f855a0fc8642a447d3c
rakeshise <rakeshise@gmail.com>
2019-01-30 17:19:02 +0530

    make it better

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT||8080);
 
 //PathLocationStradegy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
 app.get('/*', function(req,res) {
-  res.redirect(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 console.log('Console Listening'); 
