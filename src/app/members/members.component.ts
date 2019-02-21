98bb509d93fc5f0f58a5088fc72fbf4ce4cd9cb1
rahul sonone <rsonone42@gmail.com>
2019-02-21 21:58:30 +0530

    added code to save family member and refresh delete member

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,21 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IMember } from './member';
 import { Router } from '@angular/router';
 import { MembersService } from './members.service';
 
 @Component({
-    selector: 'app-members',
+  selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
 
-export class MembersComponent implements OnInit{
-    pageTitle = 'Members List';    
-    errorMessage = '';
-    members: IMember[];
-    filteredMembers: IMember[] = [];    
+export class MembersComponent implements OnInit {
+  pageTitle = 'Members List';
+  errorMessage = '';
+  members: IMember[];
+  filteredMembers: IMember[] = [];
 
-    _listFilter = '';
+  _listFilter = '';
   get listFilter(): string {
     return this._listFilter;
   }
@@ -24,33 +24,37 @@ export class MembersComponent implements OnInit{
     this.filteredMembers = this.listFilter ? this.performFilter(this.listFilter) : this.members;
   }
 
-    constructor(private router: Router,
-        private memberService: MembersService) {
-            this.getMembers();
-      }
+  constructor(private router: Router,
+    private memberService: MembersService) {
+    this.getMembers();
+  }
 
-      ngOnInit(): void {
-        //this.getMembers();
+  ngOnInit(): void {
+    //this.getMembers();
+  }
+
+  getMembers() {
+    this.memberService.getMembers().subscribe(
+      members => {
+        this.members = members;
+        this.filteredMembers = this.members;
+      },
+      error => this.errorMessage = <any>error);
+  }
+
+  performFilter(filterBy: string): IMember[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.members.filter((member: IMember) =>
+      member.Name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  onDelete(id: string) {
+    if (window.confirm('Are sure you want to delete this member ?')) {
+      this.memberService.deleteMember(id);
+      var mem = this.filteredMembers.find(x => x._id == id);
+      if (mem != null) {
+        this.filteredMembers.splice(this.filteredMembers.indexOf(mem), 1);
+      }
     }
-
-      getMembers() {
-        this.memberService.getMembers().subscribe(
-            members => {
-                this.members = members;
-                this.filteredMembers =this.members;
-        },
-          error => this.errorMessage = <any>error);
-      }
-
-      performFilter(filterBy: string): IMember[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.members.filter((member: IMember) =>
-          member.Name.toLocaleLowerCase().indexOf(filterBy) !== -1);
-      }
-
-      onDelete(id:string){
-        if(window.confirm('Are sure you want to delete this member ?')){
-            this.memberService.deleteMember(id);
-           }                
-      }
+  }
 }
